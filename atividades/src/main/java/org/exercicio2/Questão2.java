67b98bf45e69ed98a579f015e6bff5bba44812e5
wellerson <wellersonjava@gmail.com>
2024-03-21 15:14:49 -0300

    Adĩção da classe questão dois

---
+++ atividades/src/main/java/org/exercicio2/Questão2.java
@@ -0,0 +1,58 @@
+package org.exercicio2;
+
+import java.util.Scanner;
+
+public class Questão2 {
+    public int vert;
+    public int ares;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        org.example.Question1 grafo = new org.example.Question1();
+
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("Me informe a quantidade de vertices:");
+        int valor = input.nextInt();
+        grafo.setVer(valor);
+
+
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("Me informe a quantidade de arestas:");
+        valor = input.nextInt();
+        grafo.setAres(valor);
+
+        int[][] matrizincidencia = new int[grafo.vert][grafo.ares];
+        System.out.println("---------------------------------------------------------------------------");
+
+        for (int i = 0; i < grafo.vert; i++) {
+            System.out.println("O vertice " + (i + 1) + " possui arrestas? Digite 1 para verdadeiro  0 " +
+                    "para falso.");
+            valor = input.nextInt();
+
+            if (valor == 0) {
+                for (int j = 0; j < grafo.ares; j++) {
+                    matrizincidencia[i][j] = 0;
+                }
+
+            } else {
+                System.out.println("---------------------------------------------------------------------------");
+                System.out.println("Quantas arestas possui o vertice  " + (i + 1) + ":");
+                int valorAr = input.nextInt();
+
+                for (int j = 0; j < valorAr; j++) {
+                    System.out.println("Me informe o número da " + (j + 1) + "º aresta(no formato numérico) do vertice " + (i + 1) + ":");
+                    int valortemp = input.nextInt();
+                    matrizincidencia[i][valortemp - 1] = 1;
+
+                }
+            }
+        }
+
+    }
+    public void setVer(int vert) {
+        this.vert = vert;
+    }
+
+    public void setAres(int ares) {
+        this.ares = ares;
+    }
+}
